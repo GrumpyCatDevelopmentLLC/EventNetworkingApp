d8a0c9371fcebb9731196f15a57718133c9c8f56
rsampdev <rsampdev@gmail.com>
2016-10-01 19:59:45 -0400

    More UI creating events might be broken

---
+++ EventNetworkingAppTests/EventTests.swift
@@ -0,0 +1,9 @@
+//
+//  EventTests.swift
+//  EventNetworkingApp
+//
+//  Created by Rodney Sampson on 10/1/16.
+//  Copyright © 2016 GrumpyCatDevelopmentLLC. All rights reserved.
+//
+
+import Foundation
