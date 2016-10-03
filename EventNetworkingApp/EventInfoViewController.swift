4a6136da23d097f3e79d60b6bef108018c8170fa
rsampdev <rsampdev@gmail.com>
2016-10-02 20:40:23 -0400

    Creating events and viewing them is working. Checking in is not working

---
+++ EventNetworkingApp/EventInfoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EventInfoViewController.swift
+//  EventNetworkingApp
+//
+//  Created by Rodney Sampson on 10/2/16.
+//  Copyright © 2016 GrumpyCatDevelopmentLLC. All rights reserved.
+//
+
+import Foundation
